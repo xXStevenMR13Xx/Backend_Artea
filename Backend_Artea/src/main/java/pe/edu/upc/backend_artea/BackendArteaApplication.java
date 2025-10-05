a2396061d51d2a3298653e58c660cb9b987d3ca4
Kiara Hualla Martínez <u20211d621@upc.edu.pe>
2025-10-05 01:55:49 -0500

    Create Folders

---
+++ Backend_Artea/src/main/java/pe/edu/upc/backend_artea/BackendArteaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.backend_artea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendArteaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendArteaApplication.class, args);
+	}
+
+}
