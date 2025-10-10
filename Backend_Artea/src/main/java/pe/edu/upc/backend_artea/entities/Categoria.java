4a31243abd68ecedbcf528c36de4990355fb4343
Kiara Hualla Martínez <u20211d621@upc.edu.pe>
2025-10-10 00:30:33 -0500

    Creacion de entities

---
+++ Backend_Artea/src/main/java/pe/edu/upc/backend_artea/entities/Categoria.java
@@ -0,0 +1,28 @@
+package pe.edu.upc.backend_artea.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name="categorias")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_categoria;
+
+    private String nombre_categoria;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "categoria", fetch = FetchType.EAGER)
+
+    private List<Desafio> desafios;
+}
---
+++ Backend_Artea/src/main/java/pe/edu/upc/backend_artea/entities/Rol.java
@@ -0,0 +1,26 @@
+package pe.edu.upc.backend_artea.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String rol;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    @JsonIgnore
+    private User user;
+}
