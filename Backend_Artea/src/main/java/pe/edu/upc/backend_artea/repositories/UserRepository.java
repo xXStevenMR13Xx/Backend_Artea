31930e9e69808a7ba5cd4363ea8dd6a65f33e946
Kiara Hualla Martínez <u20211d621@upc.edu.pe>
2025-10-10 02:57:58 -0500

    Update

--- Backend_Artea/src/main/java/pe/edu/upc/backend_artea/repositories/UserRepository.java
+++ Backend_Artea/src/main/java/pe/edu/upc/backend_artea/repositories/UserRepository.java
@@ -1,13 +1,18 @@
 package pe.edu.upc.backend_artea.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.backend_artea.entities.User;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    public User findOneByUsername(String username);
+
     // Contar cuantos usuarios hay por rol (NATIVO)
     @Query(value =
             "SELECT r.rol AS rol, COUNT(u.id) AS total_usuarios " +
@@ -15,7 +20,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
                     "JOIN rol r ON r.usuario_id = u.id " +   // <- FK correcta
                     "GROUP BY r.rol",
             nativeQuery = true)
-    List<Object[]> countUsersByRoleNative();
+    List<String[]> QuantityUserByRol();
+
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    Optional<User> findUserById(@Param("id") int id);
 
     // contar cuantos usuarios tiene cada rol (NATIVO)
     //@Query(value = "SELECT r.rol AS Rol, COUNT(*) AS QuantityUsers " +
